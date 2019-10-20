77aa12d2945f1d53826f49def8b39e7c7a2e215c
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-20 17:34:44 +0300

    defines the url path to follow in ooder to add new projects.

--- award/urls.py
+++ award/urls.py
@@ -1,6 +1,13 @@
 from . import views 
 from django.urls import path
+from django.conf.urls import url
+from .views import  ProjectCreateView  
+
+#, ProjectUpdateView, ProjectDeleteView, ProjectListView, ProjectDetailView
+
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('Project/new/', ProjectCreateView.as_view(), name='Project-create'),
+
 ]
