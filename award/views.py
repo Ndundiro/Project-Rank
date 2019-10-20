03581ddead8704bf637208a6e75c8015309477a7
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-20 17:33:52 +0300

    Adds a project create view that allow users to add new projects

--- award/views.py
+++ award/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Project
+from django.views.generic import  ListView,DetailView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 def index(request):
     return render(request, 'award/index.html')
+
+
+
+class ProjectCreateView(CreateView):
+    model = Project
+    fields = ['sitename', 'description', 'url', 'screenshot']
+
+    def form_valid(self, form):
+        form.instance.ser = self.request.user
+        return super().form_valid(form)
