56dd4fb834b86ca75bc8087c10f8ec3299561081
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-21 07:31:23 +0300

    Defines the adding new review and individual project paths.

--- award/urls.py
+++ award/urls.py
@@ -1,13 +1,17 @@
 from . import views 
 from django.urls import path
 from django.conf.urls import url
-from .views import  ProjectCreateView  
+from .views import  ProjectCreateView, ProjectListView, ReviewCreateView, ProjectDetailView
 
-#, ProjectUpdateView, ProjectDeleteView, ProjectListView, ProjectDetailView
+#, ProjectUpdateView, ProjectDeleteView, ProjectDetailView
 
 
 urlpatterns = [
-    path('', views.index, name='home'),
-    path('Project/new/', ProjectCreateView.as_view(), name='Project-create'),
+    path('project/new/', ProjectCreateView.as_view(), name='project-create'),
+    path('', ProjectListView.as_view(), name='index'),
+    path('review/new/<int:pk>/', ReviewCreateView.as_view(), name='review-create'),
+    path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
+
+     # path('', views.index, name='home'),
 
 ]
