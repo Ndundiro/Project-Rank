06f4e128f2a2a28ebca718548bbc053596375761
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-23 15:43:27 +0300

    Adds the static url  patterns  in the urls .py to enable the display of images

--- award/urls.py
+++ award/urls.py
@@ -2,7 +2,8 @@ from . import views
 from django.urls import path
 from django.conf.urls import url
 from .views import  ProjectCreateView, ProjectListView, ReviewCreateView, ProjectDetailView
-
+from django.conf import settings
+from django.conf.urls.static import static
 #, ProjectUpdateView, ProjectDeleteView, ProjectDetailView
 
 
@@ -11,7 +12,8 @@ urlpatterns = [
     path('', ProjectListView.as_view(), name='index'),
     path('review/new/<int:pk>/', ReviewCreateView.as_view(), name='review-create'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='project-detail'),
-
-     # path('', views.index, name='home'),
-
+  # path('', views.index, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
