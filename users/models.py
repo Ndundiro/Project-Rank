36b7e4fad90a17ac675e0f7d80246d3e13f17245
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-24 14:39:46 +0300

    Fixes the nav bar and the profile page.

--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.CharField(max_length=250, default="Hey there,am on Insta", blank=True)
+    bio = models.CharField(max_length=250, default="I love awwwards,it is a source of inspiration", blank=True)
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
 
