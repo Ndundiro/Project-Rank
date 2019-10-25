39edf3274f0ed889b5c43d456cd635c628306489
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-25 13:11:09 +0300

    delpoy fix

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'award.apps.AwardConfig',
     'bootstrap3',
+    'bootstrap4'
     'crispy_forms',
     'rest_framework',
     'rest_framework.authtoken',
