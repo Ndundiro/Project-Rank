43ff9c028d166638d058a7d1ced731a333eae035
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-25 12:57:07 +0300

    Re install django bootstrap 4.

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -146,7 +146,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
