7b7af326322ffb2094619eecfe0026e7a240f1e8
Ndundiro_Kamau <ndundirokamau@gmail.com>
2019-10-25 13:14:04 +0300

    change the bootstrap version for the crispy forms from 3 to 4

--- Awwwards/settings.py
+++ Awwwards/settings.py
@@ -147,7 +147,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'index'
 LOGIN_URL = 'login'
